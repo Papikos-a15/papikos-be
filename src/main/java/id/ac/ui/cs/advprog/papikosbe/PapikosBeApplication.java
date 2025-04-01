d82ba1211474e5f600e2372120ce01f3ed370ccc
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-04-01 15:32:26 +0700

    Initialize Project

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/PapikosBeApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikosbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapikosBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapikosBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/papikosbe/PapikosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikosbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapikosBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

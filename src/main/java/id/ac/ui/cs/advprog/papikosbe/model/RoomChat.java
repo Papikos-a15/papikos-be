10979aeecb5a11067c708a0a482b0b481aef2ead
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-04-11 17:07:11 +0700

    [RED] Create Skeleton for RoomChat Model

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/model/RoomChat.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/RoomChat.java
@@ -1,4 +1,23 @@
 package id.ac.ui.cs.advprog.papikosbe.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class RoomChat {
+
+    private String id;
+    private String penyewaId;
+    private String pemilikKosId;
+    private LocalDateTime createdAt;
+
+    public RoomChat(String penyewaId, String pemilikKosId) {
+    }
+
+    public RoomChat() {}
 }

4d35d6aa58a8f7db44ae1dd61de2ee72dc9541ce
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-04-11 16:55:01 +0700

    [RED] Add wishlist service and wishlist service impl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/service/WishlistServiceImpl.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.papikosbe.service;
+
+import id.ac.ui.cs.advprog.papikosbe.model.Wishlist;
+import id.ac.ui.cs.advprog.papikosbe.repository.WishlistRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class WishlistServiceImpl implements WishlistService {
+
+    private final WishlistRepository wishlistRepository;
+
+    public WishlistServiceImpl(WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
+    }
+
+    public void addWishlist(Wishlist wishlist) {
+    }
+
+    public void removeWishlist(UUID wishlistId) {
+    }
+
+    public List<Wishlist> getAllWishlists() {
+        return null;
+    }
+}

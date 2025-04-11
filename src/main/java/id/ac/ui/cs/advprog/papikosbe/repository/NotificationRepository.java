d2ba55fa2ad545635d4d4521d11b0cff56243fd6
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-04-11 17:49:13 +0700

    [RED] Add notification repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/NotificationRepository.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.papikosbe.repository;
+
+import id.ac.ui.cs.advprog.papikosbe.model.Notification;
+
+import java.util.List;
+import java.util.UUID;
+
+public class NotificationRepository {
+
+    public Notification save(Notification notification) {
+        return null;
+    }
+
+    public Notification findById(UUID id) {
+        return null;
+    }
+
+    public List<Notification> findAll() {
+        return null;
+    }
+
+    public void deleteById(UUID id) {
+    }
+
+    public List<Notification> findByUserId(UUID userId) {
+        return null;
+    }
+}

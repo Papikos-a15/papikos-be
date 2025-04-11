db60e459f586158238adef8f0c1537756891849d
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-04-11 20:59:45 +0700

    [RED] Create Kos model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Kos.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.papikosbe.model;
+
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+public class Kos {
+
+
+    // Constructors
+    public Kos() {}
+
+    public Kos() {
+
+    }
+
+}

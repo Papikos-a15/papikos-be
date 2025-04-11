1836d851570ae82b136418f152388117d328b4c0
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-04-11 21:00:23 +0700

    [GREEN] Add Kos model implementation

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Kos.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Kos.java
@@ -6,12 +6,27 @@ import lombok.Getter;
 @Getter
 public class Kos {
 
+    // Getters
+    private Long id;
+
+    @Setter
+    private String name;
+    @Setter
+    private String address;
+    @Setter
+    private String description;
+    @Setter
+    private Double price;
+
 
     // Constructors
     public Kos() {}
 
-    public Kos() {
-
+    public Kos(String name, String address, String description, Double price) {
+        this.name = name;
+        this.address = address;
+        this.description = description;
+        this.price = price;
     }
 
 }

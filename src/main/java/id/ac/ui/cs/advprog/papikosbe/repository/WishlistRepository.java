7d1a903f0a1f44b17f4222a32cda169add4c5feb
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-04-11 16:22:28 +0700

    [GREEN] Implement wishlist repository

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/WishlistRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/WishlistRepository.java
@@ -3,25 +3,27 @@ package id.ac.ui.cs.advprog.papikosbe.repository;
 import id.ac.ui.cs.advprog.papikosbe.model.Wishlist;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class WishlistRepository {
 
+    private final Map<UUID, Wishlist> wishlistStore = new HashMap<>();
+
     public Wishlist save(Wishlist wishlist) {
-        return null;
+        wishlistStore.put(wishlist.getId(), wishlist);
+        return wishlist;
     }
 
     public Optional<Wishlist> findById(UUID wishlistId) {
-        return Optional.empty();
+        return Optional.ofNullable(wishlistStore.get(wishlistId));
     }
 
     public void deleteById(UUID wishlistId) {
+        wishlistStore.remove(wishlistId);
     }
 
     public List<Wishlist> findAll() {
-        return null;
+        return new ArrayList<>(wishlistStore.values());
     }
 }

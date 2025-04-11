5554c085233a766ac0d02e86f085459e6763d56d
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-04-11 16:06:38 +0700

    [RED] Add wishlist model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Wishlist.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.advprog.papikosbe.model;
+
+import java.util.UUID;
+
+public class Wishlist {
+
+    private UUID wishlistId;
+    private UUID userId;
+    private UUID kosId;
+
+    public Wishlist(UUID wishlistId, UUID userId, UUID kosId) {
+    }
+
+    public UUID getWishlistId() {
+        return null;
+    }
+
+    public void setWishlistId(UUID wishlistId) {
+    }
+
+    public UUID getUserId() {
+        return null;
+    }
+
+    public void setUserId(UUID userId) {
+    }
+
+    public UUID getKosId() {
+        return null;
+    }
+
+    public void setKosId(UUID kosId) {
+    }
+}

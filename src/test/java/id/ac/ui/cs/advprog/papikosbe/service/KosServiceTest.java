55934221b548941f2022a6e91cbfe1093c5d0f1b
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-04-11 20:55:15 +0700

    [RED] Create KosServiceTest skeleton

---
+++ src/test/java/id/ac/ui/cs/advprog/papikosbe/service/KosServiceTest.java
@@ -0,0 +1,42 @@
+package id.ac.ui.cs.advprog.papikosbe.service;
+
+import id.ac.ui.cs.advprog.papikosbe.repository.KosRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class KosServiceTest {
+
+    @Mock
+    private KosRepository kosRepository;
+
+    @InjectMocks
+    private KosService kosService;
+
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void testAddKos() {
+    }
+
+    @Test
+    public void testGetAllKos() {
+    }
+
+    @Test
+    public void testGetKosById() {
+    }
+
+    @Test
+    public void testUpdateKos() {
+    }
+
+    @Test
+    public void testDeleteKos() {
+    }
+}
+

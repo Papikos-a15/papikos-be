581c6b891487a4dd548e4e55998a912117842379
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-04-11 19:19:52 +0700

    [RED] Add Message model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Message.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.papikosbe.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Message {
+    private UUID id;
+    private UUID roomChatId;
+    private UUID senderId;
+    private String content;
+    private LocalDateTime timestamp;
+    private boolean isEdited;
+
+    public Message(UUID roomChatId, UUID senderId, String content) {
+    }
+}

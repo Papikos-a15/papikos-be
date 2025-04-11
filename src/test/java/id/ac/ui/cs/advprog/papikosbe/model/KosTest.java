5690ba3335c7b1c33508caedb52ee1fd1ccd1830
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-04-11 20:58:24 +0700

    [RED] Create KosTest skeleton

---
+++ src/test/java/id/ac/ui/cs/advprog/papikosbe/model/KosTest.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.papikosbe.model;
+
+import org.junit.jupiter.api.Test;
+
+
+public class KosTest {
+
+    @Test
+    public void testDefaultConstructorAndSetters() {
+
+    }
+
+    @Test
+    public void testParameterizedConstructor() {
+
+    }
+}

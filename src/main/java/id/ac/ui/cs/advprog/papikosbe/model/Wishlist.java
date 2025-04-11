fd78c013959dea96e7ab3da3c52d0045badf3a45
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-04-11 16:08:10 +0700

    [GREEN] Implement wishlist model

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Wishlist.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/Wishlist.java
@@ -9,26 +9,35 @@ public class Wishlist {
     private UUID kosId;
 
     public Wishlist(UUID wishlistId, UUID userId, UUID kosId) {
+        if (wishlistId == null || userId == null || kosId == null) {
+            throw new IllegalArgumentException("Wishlist ID, User ID, and Kos ID cannot be null");
+        }
+        this.wishlistId = wishlistId;
+        this.userId = userId;
+        this.kosId = kosId;
     }
 
-    public UUID getWishlistId() {
-        return null;
-    }
-
-    public void setWishlistId(UUID wishlistId) {
+    public UUID getId() {
+        return wishlistId;
     }
 
     public UUID getUserId() {
-        return null;
-    }
-
-    public void setUserId(UUID userId) {
+        return userId;
     }
 
     public UUID getKosId() {
-        return null;
+        return kosId;
+    }
+
+    public void setWishlistId(UUID wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
     }
 
     public void setKosId(UUID kosId) {
+        this.kosId = kosId;
     }
 }

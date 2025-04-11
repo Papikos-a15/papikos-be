60a4dc843885fddc15d66251fbdbbf72698cb2db
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-04-11 20:00:51 +0700

    [GREEN] Implement MessageRepository class

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/MessageRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/MessageRepository.java
@@ -12,19 +12,25 @@ public class MessageRepository {
     private final Map<UUID, Message> messageStore = new HashMap<>();
 
     public void createMessage(Message message) {
+        messageStore.put(message.getId(), message);
     }
 
     public Message getMessageById(UUID id) {
-        return null;
+        return messageStore.get(id);
     }
 
     public void editMessage(Message updatedMessage) {
+        messageStore.put(updatedMessage.getId(), updatedMessage);
     }
 
     public boolean deleteMessage(UUID id) {
-        return false;
+        return messageStore.remove(id) != null;
     }
 
     public List<Message> getMessagesByRoomId(UUID roomId) {
+        return messageStore.values().stream()
+                .filter(message -> message.getRoomChatId().equals(roomId))
+                .sorted(Comparator.comparing(Message::getTimestamp))
+                .collect(Collectors.toList());
     }
 }

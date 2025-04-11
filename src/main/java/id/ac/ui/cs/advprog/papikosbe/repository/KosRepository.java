0c02bc57f1c7e6e8ab5b07f380fe1ce7e6856502
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-04-11 21:01:09 +0700

    [RED] Add KosRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/KosRepository.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.papikosbe.repository;
+
+import org.springframework.stereotype.Repository;
+import id.ac.ui.cs.advprog.papikosbe.model.Kos;
+
+@Repository
+public class KosRepository {
+}

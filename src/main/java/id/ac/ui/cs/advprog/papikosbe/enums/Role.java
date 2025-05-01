22372f704557691589012644ce83a088135d6709
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-05-01 15:32:41 +0700

    Add Role enum and update build.gradle.kts for JPA and PostgreSQL dependencies

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/enums/Role.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.papikosbe.enums;
+
+public enum Role {
+    ADMIN,
+    OWNER,
+    TENANT
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/User.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.papikosbe.model;
+
+import id.ac.ui.cs.advprog.papikosbe.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String fullName;
+    private String phone;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean verified = false;
+}

4bad7a8fd4d9a84389fb0092372fdf7238bc6049
naylafn <naylafarah.31@gmail.com>
2025-05-02 17:59:23 +0700

    [GREEN] Implement Payment controller

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/service/PaymentService.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.papikosbe.service;
 
 import id.ac.ui.cs.advprog.papikosbe.model.Payment;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
--- src/main/java/id/ac/ui/cs/advprog/papikosbe/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/service/PaymentServiceImpl.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.papikosbe.service;
 
 import id.ac.ui.cs.advprog.papikosbe.factory.PaymentFactory;
+import id.ac.ui.cs.advprog.papikosbe.factory.TransactionFactory;
 import id.ac.ui.cs.advprog.papikosbe.model.Payment;
 import org.springframework.stereotype.Service;
+
+import id.ac.ui.cs.advprog.papikosbe.enums.TransactionType;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,13 +16,22 @@ public class PaymentServiceImpl implements PaymentService {
 
     private final Map<UUID, Payment> paymentRepository = new HashMap<>();
     private final PaymentFactory paymentFactory;
+    private final TransactionFactory transactionFactory;
+    private final TransactionService transactionService;
 
-    public PaymentServiceImpl(PaymentFactory paymentFactory) {
+    public PaymentServiceImpl(PaymentFactory paymentFactory, TransactionFactory transactionFactory, TransactionService transactionService) {
         this.paymentFactory = paymentFactory;
+        this.transactionFactory = transactionFactory;
+        this.transactionService = transactionService;
     }
 
     public Payment createPayment(UUID userId, UUID ownerId, BigDecimal amount) {
         Payment payment = paymentFactory.createPayment(userId, ownerId, amount);
+
+        var transaction = transactionFactory.createTransaction(
+                userId, amount, TransactionType.PAYMENT
+        );
+        transactionService.create(transaction);
         return create(payment);
     }
 

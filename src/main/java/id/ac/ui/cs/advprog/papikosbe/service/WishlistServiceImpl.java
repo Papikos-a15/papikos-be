72b2453105c9dd8055597d868c5667d0c18e005e
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-05-02 16:11:36 +0700

    [REFACTOR] Fix WishlistServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/service/WishlistServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/service/WishlistServiceImpl.java
@@ -2,10 +2,12 @@ package id.ac.ui.cs.advprog.papikosbe.service;
 
 import id.ac.ui.cs.advprog.papikosbe.model.Wishlist;
 import id.ac.ui.cs.advprog.papikosbe.repository.WishlistRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class WishlistServiceImpl implements WishlistService {
 
     private final WishlistRepository wishlistRepository;

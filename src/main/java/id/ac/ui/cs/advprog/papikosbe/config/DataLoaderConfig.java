f582538085e024cf3ce920c655a55d0b6434d148
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-05-11 11:01:13 +0700

    feat: add DataLoaderConfig to initialize admin user on startup

---
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/config/DataLoaderConfig.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.papikosbe.config;
+
+import id.ac.ui.cs.advprog.papikosbe.enums.Role;
+import id.ac.ui.cs.advprog.papikosbe.model.user.Admin;
+import id.ac.ui.cs.advprog.papikosbe.model.user.User;
+import id.ac.ui.cs.advprog.papikosbe.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.boot.CommandLineRunner;
+
+@Configuration
+public class DataLoaderConfig {
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner loadData(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return (args) -> {
+            if (userRepository.findByEmail("admin1@example.com").isEmpty()) {
+                User admin = new Admin();
+                admin.setEmail("admin1@example.com");
+                admin.setPassword(passwordEncoder.encode(adminPassword));
+                admin.setRole(Role.ADMIN);
+                userRepository.save(admin);
+            }
+        };
+    }
+}

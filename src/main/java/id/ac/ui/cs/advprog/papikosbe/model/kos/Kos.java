083f6b4f55372ace266e30794217b84d0c882f05
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2025-05-15 22:39:16 +0700

    [GREEN] Add database connection to Kos model

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/model/kos/Kos.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/kos/Kos.java
@@ -1,21 +1,40 @@
 package id.ac.ui.cs.advprog.papikosbe.model.kos;
 
-import lombok.Setter;
-import lombok.Getter;
+import lombok.*;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
-@Setter
-@Getter
+@Entity
+@Table(name = "kos")
+@Builder
+@Setter @Getter
 public class Kos {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "kos_id", columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID id;
+
+    @Column(name = "owner_id", updatable = false, nullable = false)
     private UUID ownerId;
+
+    @Column(name = "tenant_id", updatable = false, nullable = false)
     private UUID tenantId;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "address", nullable = false)
     private String address;
+
+    @Column(name = "description", nullable = true)
     private String description;
+
+    @Column(name = "price", nullable = false)
     private Double price;
+
+    @Column(name = "is_available", nullable = false)
     private boolean isAvailable;
 
 

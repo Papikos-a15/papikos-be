5c3089d05851e7493f601f775bd92597874d1ff8
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-05-19 11:00:38 +0700

    [REFACTOR] RoomChat entity with JPA annotations

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/model/chat/RoomChat.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/model/chat/RoomChat.java
@@ -1,18 +1,31 @@
 package id.ac.ui.cs.advprog.papikosbe.model.chat;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "room_chats")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoomChat {
 
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
+
+    @Column(name = "penyewa_id", nullable = false, updatable = false)
     private UUID penyewaId;
+
+    @Column(name = "pemilik_kos_id", nullable = false, updatable = false)
     private UUID pemilikKosId;
+
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     public RoomChat(UUID penyewaId, UUID pemilikKosId) {

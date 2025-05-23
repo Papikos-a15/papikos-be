64fa213ec35e5036b83b4c307aa5e2e5081e7bfb
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2025-05-23 09:53:35 +0700

    [GREEN] Implement finding RoomChat by penyewaId and pemilikKosId in RoomChatRepository

--- src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/chat/RoomChatRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/papikosbe/repository/chat/RoomChatRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RoomChatRepository extends JpaRepository<RoomChat, UUID> {
     List<RoomChat> findAllByPenyewaId(UUID penyewaId);
     List<RoomChat> findAllByPemilikKosId(UUID pemilikKosId);
+    Optional<RoomChat> findByPenyewaIdAndPemilikKosId(UUID penyewaId, UUID pemilikKosId);
 }
